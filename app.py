a76eebdbd9901b967aeabcdadd4453a3eae82e6b
myasinrc-dot <email-github-myasinrc@gmail.com>
2026-09-19 18:19:25 +0800

    Ubah port ke dinamis

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -29,5 +30,7 @@ def handle_send_message(data):
     emit('terima_pesan', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+    # Mengambil port otomatis dari Render, atau default ke 5000
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
 
